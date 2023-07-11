a44cafac90a702fd46ca26063d3650412a3e6267
KumaCatz <32141751+KumaCatz@users.noreply.github.com>
2023-07-11 15:03:24 +0300

    M2 done

--- src/components/GenerateNote.jsx
+++ src/components/GenerateNote.jsx
@@ -3,11 +3,14 @@ import "../css/GenerateNote.css";
 
 function GenerateNote({ notes }) {
     const [notesList, setNotesList] = useState(notes);
-    
+
     function handleDelete(id) {
         if (window.confirm('Delete this note?') == true) {
-            const updatedNotes = notes.splice(id, 1);
+            const index = notes.slice().map(element => element.id).indexOf(id);
+            console.log('index ' + index)
+            const updatedNotes = notes.splice(index, 1);
             setNotesList(updatedNotes);
+            console.log(notes)
         }
 
     }
